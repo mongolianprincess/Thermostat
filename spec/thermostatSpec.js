58e281956de41f5af50a886b70ac8a27a2c931f2
mongolianprincess <ayc.yang@gmail.com>
2015-11-11 16:09:22 +0000

    Added getCurrentTemperature method

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -1,11 +1,13 @@
 describe('Thermostat', function() {
 
+  'use strict';
+
   var thermostat;
   beforeEach(function() {
     thermostat = new Thermostat();
   });
 
   it('has temperaturature starting at 20 degrees', function() {
-    expect(thermostat.temperature).toEqual(20);
+    expect(thermostat.getCurrentTemperature()).toEqual(20);
   });
 });
