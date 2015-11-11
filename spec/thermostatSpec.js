3b27a60c92eb200c9056d2774d25f065af26865d
mongolianprincess <ayc.yang@gmail.com>
2015-11-11 16:23:45 +0000

    Added down method to decrease temperature

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -11,8 +11,13 @@ describe('Thermostat', function() {
     expect(thermostat.getCurrentTemperature()).toEqual(20);
   });
 
-  it('increases temperaturature by 1', function() {
+  it('increases temperaturature by 1 with up button', function() {
     thermostat.up();
     expect(thermostat.getCurrentTemperature()).toEqual(21);
+  });
+
+  it ('decreases temperature by 1 with down button',function(){
+    thermostat.down();
+    expect(thermostat.getCurrentTemperature()).toEqual(19);
   });
 });
