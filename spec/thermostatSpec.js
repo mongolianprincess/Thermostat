73f7643bb56ee909804a7439fd920c49d9f15e26
“DovileSand” <dovile.sand@protonmail.ch>
2015-11-11 17:50:37 +0000

    Does not allow to decrease temp below minimum temperature

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -16,8 +16,19 @@ describe('Thermostat', function() {
     expect(thermostat.getCurrentTemperature()).toEqual(21);
   });
 
-  it ('decreases temperature by 1 with down button',function(){
+  it ('decreases temperature by 1 with down button',function() {
     thermostat.down();
     expect(thermostat.getCurrentTemperature()).toEqual(19);
   });
+
+  it ('has a minimum temperature of 10 degrees', function() {
+    expect(thermostat.getMinTemp()).toEqual(10);
+  });
+
+
+  // it ('throws error when reaching minimum temperature', function() {
+  //   for (i = 0; i < 11; i++) {thermostat.down();}
+  //   expect(thermostat.getCurrentTemperature()).toThrow(new Error("Minimum temperature is 10"));
+  // });
+
 });
