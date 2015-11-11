6d211b72dcda1603f79ab3c1ce66baad4836225e
“DovileSand” <dovile.sand@protonmail.ch>
2015-11-11 15:13:32 +0000

    Add default temperature constant

---
+++ spec/thermostatSpec.js
@@ -0,0 +1,11 @@
+describe('Thermostat', function() {
+
+  var thermostat;
+  beforeEach(function() {
+    thermostat = new Thermostat();
+  });
+
+  it('has temperaturature starting at 20 degrees', function() {
+    expect(thermostat.temperature).toEqual(20);
+  });
+});
